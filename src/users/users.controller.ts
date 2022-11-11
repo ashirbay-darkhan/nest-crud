3527319babd0be7f54c1165182e2b819dd718475
darkhan <darkhan.ashirbay01@gmail.com>
2022-11-11 18:14:25 +0600

    nest crud

---
+++ src/users/users.controller.ts
@@ -0,0 +1,52 @@
+import {Body, Controller, Delete, Get, Param, Post, Put} from "@nestjs/common";
+import {UsersService} from "./users.service";
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  getUsers() {
+    return this.userService.getUsers()
+  }
+
+  @Post()
+  insertUser(
+    @Body('name') name: string,
+    @Body('age') age: number,
+    @Body('surname') surname: string,
+    @Body('email') email: string,
+  ) {
+    const userId = this.userService.insertUser(name, age, surname, email)
+    return {
+      id: userId,
+    }
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getUsers()
+  }
+
+  @Get(':userId')
+  getUser(@Param('userId') userId: string) {
+    return this.userService.getUser(userId)
+  }
+
+  @Put(':userId')
+  updateUser(
+    @Param('userId') userId: string,
+    @Body('name') name: string,
+    @Body('age') age: number,
+    @Body('surname') surname: string,
+    @Body('email') email: string,
+  ) {
+    return this.userService.updateUser(userId, name, age, surname, email)
+  }
+
+  @Delete(':userId')
+  deleteUser(@Param('userId') userId: string) {
+    this.userService.deleteUser(userId)
+  }
+
+}
